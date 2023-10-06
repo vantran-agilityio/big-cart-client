243d24d67fb3ddfaac6e50e830b444a082ee7dbc
Van Tran <van.tran@asnet.com.vn>
2023-10-06 10:12:30 +0700

    Config switch between app and storybook

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   );
+};
+
+let AppEntryPoint = App;
+
+if (Constants?.expoConfig?.extra?.storybookEnabled === 'true') {
+  AppEntryPoint = require('./.storybook').default;
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 });
+
+export default AppEntryPoint;
--- app.config.js
+++ app.config.js
@@ -1,10 +1,31 @@
-module.exports = () => {
-  if (process.env.STORYBOOK_ENABLED) {
-    console.log('process.env.STORYBOOK_ENABLED', process.env.STORYBOOK_ENABLED);
-    return require('./app.storybook.json');
+module.exports = {
+  expo: {
+    name: 'react-training',
+    slug: 'react-training',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      }
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    },
+    extra: {
+      storybookEnabled: process.env.STORYBOOK_ENABLED
+    }
   }
-
-  console.log('out',);
-
-  return require('./app.json');
 };
